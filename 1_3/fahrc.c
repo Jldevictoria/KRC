d57fae04e57d6423468a8b4144aa347d4abee832
Joseph DeVictoria <jldevictoria@gmail.com>
2016-09-02 12:36:10 -0700

    Added gitignore and Exercise 1.3.

--- 1_3/fahrc.c
+++ 1_3/fahrc.c
@@ -9,4 +9,17 @@
 // for fahr = 0, 20, ..., 300; floating point version.
 
 main() {
+    float fahr;
+    float celsius;
+    int lower = 0;
+    int upper = 300;
+    int step = 20;
+
+    printf("Fahrenheit Celsius\n");
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%10.0f %7.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
 }
